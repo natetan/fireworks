17ebcb8542725cb8e8b50f8d562d0767da8b617b
Yulong Tan <yulongtan67@gmail.com>
2017-06-27 13:21:50 -0700

    new view controller

---
+++ Fireworks/AboutViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Fireworks
+{
+    public partial class AboutViewController : UIViewController
+    {
+        public AboutViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
